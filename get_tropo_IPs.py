cdb5f92f26801dd63b3099ee699fccb48da41a13
Vince <amazingvince@gmail.com>
2016-11-17 11:42:10 -0500

    Added some error handling and fixed clean_ip to run on windows

--- get_tropo_IPs.py
+++ get_tropo_IPs.py
@@ -1,25 +1,37 @@
 import subprocess
 import shlex
 
-Prefix = "tropo"  # This will proceed the IP address
-Command = "nslookup -q=TXT _netblocks.tropo.com 8.8.8.8"
 
 """
-This script gets IP addresses then formats and sends them to STDOUT.
+If there are multiple service you need to this information for this scrip is easily expandable 
+and reusable by adding sys argv for command and prefix.
+
+This script gets IP addresses to whitelist then formats and sends them to STDOUT.
+
+Expected output format:
+    Prefix + , + IP
+
 Example:
     tropo,10.10.0.10/24
-    Prefix + , + IP
 """
 
+Prefix = "tropo"  # This will proceed the IP address
+Command = "nslookup -q=TXT _netblocks.tropo.com 8.8.8.8"
 
 def get_ips(command):
     p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     ips, err = p.communicate()
-    return ips
-
+    if ips:
+        return ips
+    if err:
+        print "standard error of subprocess:"
+        print err
 
 def clean_ip(line, prefix):
-    return prefix + "," + line[line.find('"') + 1:-1]
+    # rearanged line processing to work cross platform
+    ip = line[line.find('"') + 1:] 
+    ip = ip[:ip.find('"')]
+    return prefix + "," + ip
 
 
 def process_ips(ips, prefix):
